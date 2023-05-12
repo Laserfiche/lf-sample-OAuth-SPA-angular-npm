5cd082f9a749560284665a12fc3dde796b176965
rfulton-lf <robert.fulton@laserfiche.com>
2023-05-12 10:32:28 -0700

    formatted config file

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,8 +1,8 @@
 const config = {
-    REDIRECT_URI: 'REPLACE_WITH_YOUR_REDIRECT_URI', // i.e http://localhost:3000, https://serverName/lf-sample/index.html
+  REDIRECT_URI: 'REPLACE_WITH_YOUR_REDIRECT_URI', // i.e http://localhost:3000, https://serverName/lf-sample/index.html
   CLIENT_ID: 'REPLACE_WITH_YOUR_CLIENT_ID',
   HOST_NAME: 'laserfiche.com', // only update this if you are using a different environment (i.e. a.clouddev.laserfiche.com)
-  SCOPE:'repository.Read repository.Write', // Scope(s) requested by the app
-}
+  SCOPE: 'repository.Read repository.Write', // Scope(s) requested by the app
+};
 
 export default config;
