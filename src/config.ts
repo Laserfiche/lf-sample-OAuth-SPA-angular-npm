4b31e535af0c5d20d6af20da208ef199d05cc818
anirudh-satish <141184676+anirudh-satish@users.noreply.github.com>
2023-08-16 16:15:04 -0700

    new version of config.ts added

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 const config = {
   REDIRECT_URI: 'REPLACE_WITH_YOUR_REDIRECT_URI', // i.e http://localhost:3000, https://serverName/lf-sample/index.html
+  REDIRECT_URI_POPUP: 'REPLACE_WITH_YOUR_REDIRECT_URI', // i.e http://localhost:3000/static-assets/login.html, https://serverName/lf-sample/static-assets/login.html 
   CLIENT_ID: 'REPLACE_WITH_YOUR_CLIENT_ID',
   HOST_NAME: 'laserfiche.com', // only update this if you are using a different environment (i.e. a.clouddev.laserfiche.com)
   SCOPE: 'repository.Read repository.Write', // Scope(s) requested by the app
