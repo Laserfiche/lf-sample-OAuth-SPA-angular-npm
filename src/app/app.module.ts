ac8b09b856bc87055a65e41a252c51eb935703fd
Anirudh Satish <anirudh.satish@laserfiche.com>
2023-09-15 16:56:57 -0700

    updaetd packages and html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { LfBreadcrumbsModule } from '@laserfiche/lf-ui-components/shared';
 import { NewFolderModalComponent } from './new-folder-modal/new-folder-modal.component';
 import { EditColumnsModalComponent } from './edit-columns-modal/edit-columns-modal.component';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MatLegacySelectModule as MatSelectModule } from '@angular/material/legacy-select';
+import { MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [
