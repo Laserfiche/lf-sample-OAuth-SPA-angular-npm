d1d6579cde272e4c737d637c0408ac5fd42fd108
Paolo Argentieri <paolo.argentieri@laserfiche.com>
2026-05-29 16:14:21 -0700

    npm update

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 const config = {
-  REDIRECT_URI: 'REPLACE_WITH_YOUR_REDIRECT_URI', // i.e http://localhost:3000, https://serverName/lf-sample  Register both REDIRECT_URL and REDIRECT_URI/login.html in devconsole
-  CLIENT_ID: 'REPLACE_WITH_YOUR_CLIENT_ID',
+  REDIRECT_URI: 'http://127.0.0.1:4200', // i.e http://localhost:3000, https://serverName/lf-sample  Register both REDIRECT_URL and REDIRECT_URI/login.html in devconsole
+  CLIENT_ID: 'e89e9177-1910-4027-8eac-4e8691431505',
   HOST_NAME: 'laserfiche.com', // only update this if you are using a different environment (i.e. a.clouddev.laserfiche.com)
   SCOPE: 'repository.Read repository.Write', // Scope(s) requested by the app
 };
